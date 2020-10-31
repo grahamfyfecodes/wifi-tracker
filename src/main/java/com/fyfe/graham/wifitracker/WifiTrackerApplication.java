84cda7e85cba0565ca19232820b038e24561425d
Graham Fyfe <grahamdavidfyfe@gmail.com>
2020-10-31 18:00:29 +0000

    Init commit

---
+++ src/main/java/com/fyfe/graham/wifitracker/WifiTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fyfe.graham.wifitracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyfe/graham/wifitracker/WifiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyfe.graham.wifitracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
